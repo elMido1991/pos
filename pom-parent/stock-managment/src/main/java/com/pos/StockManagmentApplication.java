70d1ce3bf355ae5e2b98b5eb452bf1044e17708e
el_mehdi <mehdi.azoui@gmail.com>
2019-10-19 22:47:52 +0100

    update POM projects

--- pom-parent/stock-managment/src/main/java/com/pos/StockManagmentApplication.java
+++ pom-parent/stock-managment/src/main/java/com/pos/StockManagmentApplication.java
@@ -1,13 +1,20 @@
 package com.pos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StockManagmentApplication {
+public class StockManagmentApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockManagmentApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
