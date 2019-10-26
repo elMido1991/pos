667b22c689a27db1deba2be0908288e3b854b896
el mehdi azoui <mehdi.azoui@gmail.com>
2019-10-26 15:00:28 +0200

    Add Client Registry main Application

---
+++ pom-parent/client-registry/src/main/java/com/pos/ClientRegistryApplication.java
@@ -0,0 +1,20 @@
+package com.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@SpringBootApplication
+@EnableSwagger2
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ClientRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientRegistryApplication.class, args);
+	}
+}
