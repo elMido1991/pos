12cad485b1b37f0d3492322ee9c7b25ec7b115fe
el mehdi azoui <mehdi.azoui@gmail.com>
2019-10-28 19:55:31 +0000

    update Controller Interface

--- pom-parent/stock-managment/src/main/java/com/pos/controller/IStockController.java
+++ pom-parent/stock-managment/src/main/java/com/pos/controller/IStockController.java
@@ -1,9 +1,26 @@
 package com.pos.controller;
 
-import com.pos.service.IEntrepotService;
-import com.pos.service.ILocalisationService;
-import com.pos.service.IStockService;
+import java.util.Map;
+import java.util.Set;
 
-public interface IStockController extends IEntrepotService,IStockService,ILocalisationService {
+import com.pos.dao.model.dto.EntrepotDto;
+import com.pos.dao.model.dto.LocalisationDto;
+import com.pos.dao.model.dto.StockDto;
 
+public interface IStockController {
+	EntrepotDto addEntrepot(EntrepotDto entrepotDto);
+	EntrepotDto updateEntrepot(long idEntrepot, EntrepotDto entrepotDto);
+	void deleteEntrepot(long idEntrepot);
+	EntrepotDto getEntrepot(long idEntrepot);
+	Set<EntrepotDto> getEntrepots(Long ...idEntrepot);
+	StockDto addQuantityToStock(StockDto stockDto);
+	StockDto updateQuantityOfStock(StockDto stockDto, double quantity);
+	StockDto removeQuantityFromStock(StockDto stockDto, double quantity);
+	StockDto getStockOfArticle(long idArticle, long idEntrepot);
+	Set<StockDto> getStockOfArticles(Map<Long, Long> listStocks);
+	LocalisationDto addLocalisation(LocalisationDto LocalisationDto);
+	LocalisationDto updateLocalisation(long idLocalisation, LocalisationDto LocalisationDto);
+	void deleteLocalisation(long idLocalisation);
+	LocalisationDto getLocalisation(long idLocalisation);
+	Set<LocalisationDto> getLocalisations(Long ...idLocalisation);
 }
